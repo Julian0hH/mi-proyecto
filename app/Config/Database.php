9c06ea79a3a45dade39be5dfdbf236947bbe46f4
Julian Hernandez <hernandez.hjose083.2023@gmail.com>
2026-01-20 16:00:52 -0600

    Add encrypt

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -17,6 +17,7 @@ class Database extends Config
         'database'     => '',
         'schema'       => 'public',
         'DBDriver'     => 'Postgre',
+        'encrypt'  => true,
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
