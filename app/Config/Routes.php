0e67b99a00319f7d96d9c43f9c0a0b7d1508aa65
Julian Hernandez <hernandez.hjose083.2023@gmail.com>
2026-01-27 01:42:46 -0600

    Refactor routing in Routes.php: streamline route definitions and improve consistency

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,14 +1,19 @@
 <?php
+
 use CodeIgniter\Router\RouteCollection;
 
+/**
+ * @var RouteCollection $routes
+ */
+
 $routes->get('/', 'Home::index');
-$routes->post('guardar_mensaje', 'Home::guardar_mensaje');
+
 $routes->get('registro', 'Home::registro');
-$routes->post('guardar_usuario', 'Home::guardar_usuario');
-$routes->get('eliminar/(:num)', 'Home::eliminar/$1');
+$routes->post('guardar', 'Home::guardar');
+$routes->get('eliminar/(:any)', 'Home::eliminar/$1');
 $routes->get('servicios', 'Home::servicios');
 $routes->get('detalles', 'Home::detalles');
+$routes->get('contratar', 'Home::contratar');
 $routes->get('validacion', 'Home::validacion');
 $routes->post('procesar_validacion', 'Home::procesar_validacion');
 $routes->get('prueba_error', 'Home::prueba_error');
-$routes->get('contratar', 'Home::contratar');
